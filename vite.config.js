89fd14d8675e4795fef41d06adeb6cdd953fbf16
vladius <vladpsico@gmail.com>
2024-08-05 16:59:56 -0600

    chore: Actualizar configuración de Vite para cargar variables de entorno

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    transform: {
-      '^.+\\.jsx?$': 'esbuild-jest',
+export default ({ mode }) => {
+  // Cargar variables de entorno
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [react()],
+    define: {
+      // Pasar las variables de entorno a la configuración de Vite
+      'process.env': {
+        VITE_API_URL: env.VITE_API_URL,
+      },
     },
-  },
-})
+    test: {
+      transform: {
+        "^.+\\.jsx?$": "esbuild-jest",
+      },
+    },
+  });
+};
